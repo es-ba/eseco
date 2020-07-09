9d31d89b2701fac84f0f85ed3f03517376b6a97f
Emilio Platzer <emilioplatzer@gmail.com>
2020-07-08 22:52:29 -0300

    Faltaban lotes y semanas

---
+++ src/server/table-lotes.ts
@@ -0,0 +1,34 @@
+"use strict";
+
+import {TableDefinition, TableContext} from "./types-eseco";
+
+export function lotes(context:TableContext):TableDefinition {
+    var admin = context.user.rol==='admin';
+    return {
+        name:'lotes',
+        elementName:'lote',
+        editable:admin,
+        fields:[
+            {name:'lote'            , typeName:'integer'   },
+            {name:'barrios'         , typeName:'text'      , isName:true     },
+            {name:'encuestas'       , typeName:'integer'   , isName:true     },
+        ],
+        primaryKey:['lote'],
+        detailTables:[
+            {table:'tem'          , fields:['lote'], abr:'T', label:'TEM'},
+        ],
+        sql:{
+            from:`(
+                select lote, 
+                        string_agg(distinct barrio, ', ' order by barrio) as barrios, 
+                        count(*) as encuestas 
+                    from tem
+                    where lote is not null
+                    group by lote
+                    order by lote
+            )`,
+            isTable:false
+        }
+    };
+}
+
---
+++ src/server/table-semanas.ts
@@ -0,0 +1,32 @@
+"use strict";
+
+import {TableDefinition, TableContext} from "./types-eseco";
+
+export function semanas(context:TableContext):TableDefinition {
+    var admin = context.user.rol==='admin';
+    return {
+        name:'semanas',
+        elementName:'semana',
+        editable:admin,
+        fields:[
+            {name:'semana'          , typeName:'integer'   },
+            {name:'encuestas'       , typeName:'integer'   , isName:true     },
+        ],
+        primaryKey:['semana'],
+        detailTables:[
+            {table:'tem'          , fields:['semana'], abr:'T', label:'TEM'},
+        ],
+        sql:{
+            from:`(
+                select semana, 
+                        count(*) as encuestas 
+                    from tem
+                    where semana is not null
+                    group by semana
+                    order by semana
+            )`,
+            isTable:false
+        }
+    };
+}
+
