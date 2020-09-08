440de2d9bce76e622ea56a0d637ee1ff866d8486
manueldelapenna <manueldelapenna@gmail.com>
2020-09-08 13:36:20 -0300

    trigger que copia resumen estado a tareas tem

---
+++ install/sincronizacion_resumen_estado_tem.sql
@@ -0,0 +1,28 @@
+﻿set role to eseco202_produc_owner;
+set search_path = encu;
+
+CREATE OR REPLACE FUNCTION encu.sincronizacion_resumen_estado_tem_trg()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+AS $BODY$
+declare
+  resumen_estado         text=new.resumen_estado;
+begin
+	update tareas_tem tt 
+		set resultado = 
+			(case 
+				when resumen_estado is distinct from 'vacio' and resumen_estado is distinct from 'no rea' then resumen_estado  
+				when resumen_estado = 'no rea' then COALESCE(tt.resultado,'') || '-' ||resumen_estado
+				else null
+			end)
+		where tt.operativo = NEW.operativo and tt.enc = NEW.enc;
+    return new;
+end;
+$BODY$;
+
+drop trigger if exists sincronizacion_resumen_estado_tem_trg on tem;
+CREATE TRIGGER sincronizacion_resumen_estado_tem_trg
+  BEFORE INSERT OR UPDATE OF resumen_estado
+  ON tem
+  FOR EACH ROW
+  EXECUTE PROCEDURE sincronizacion_resumen_estado_tem_trg();
