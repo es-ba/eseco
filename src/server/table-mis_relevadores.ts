4c1fcec61c12bab2695268e0efd0705def86ee81
manueldelapenna <manueldelapenna@gmail.com>
2020-09-08 11:30:41 -0300

    comento provisoriamente campos que se calculan con la fecha del área (campo que por ahora no existe)

--- src/server/table-mis_relevadores.ts
+++ src/server/table-mis_relevadores.ts
@@ -21,15 +21,15 @@ export function mis_relevadores(context:TableContext):TableDefinition {
             {name:'mail'             , typeName:'text'                      },
             {name:'mail_alternativo' , typeName:'text'                      },
             {name:'ultima_sincro'           , typeName:'timestamp', editable:false },
-            {name:'carga_hoy'               , typeName:'integer' , editable:false  , aggregate:'sum'},
-            {name:'carga_1'                 , typeName:'integer' , editable:false  , aggregate:'sum'},
-            {name:'carga_2'                 , typeName:'integer' , editable:false  , aggregate:'sum'},
-            {name:'carga_3'                 , typeName:'integer' , editable:false  , aggregate:'sum'},
+            //{name:'carga_hoy'               , typeName:'integer' , editable:false  , aggregate:'sum'},
+            //{name:'carga_1'                 , typeName:'integer' , editable:false  , aggregate:'sum'},
+            //{name:'carga_2'                 , typeName:'integer' , editable:false  , aggregate:'sum'},
+            //{name:'carga_3'                 , typeName:'integer' , editable:false  , aggregate:'sum'},
             {name:'reas_m'                  , typeName:'integer' , editable:false  , aggregate:'sum'},
             {name:'no_reas'                 , typeName:'integer' , editable:false  , aggregate:'sum'},
             {name:'incompletas'             , typeName:'integer' , editable:false  , aggregate:'sum'},
             {name:'vacias'                  , typeName:'integer' , editable:false  , aggregate:'sum'},
-            {name:'reas_dia'                , typeName:'decimal' , editable:false  , title:'reas/día' , aggregate:'avg'},
+            //{name:'reas_dia'                , typeName:'decimal' , editable:false  , title:'reas/día' , aggregate:'avg'},
             {name:'tarea'                   , typeName:'text'    , editable:false  },
         ],
         primaryKey:['relevador'],
@@ -42,15 +42,15 @@ export function mis_relevadores(context:TableContext):TableDefinition {
             from:`(
                 select u.idper as relevador, u.*, t.*, s.*, 'rel' as tarea
                     from usuarios u, lateral (
-                        select sum(case when a.fecha = current_date then 1 else null end) as carga_hoy,
-                                sum(case when a.fecha = current_date + interval '1 day' then 1 else null end) as carga_1,
-                                sum(case when a.fecha = current_date + interval '2 day' then 1 else null end) as carga_2,
-                                sum(case when a.fecha = current_date + interval '3 day' then 1 else null end) as carga_3,
+                        select --sum(case when a.fecha = current_date then 1 else null end) as carga_hoy,
+                                --sum(case when a.fecha = current_date + interval '1 day' then 1 else null end) as carga_1,
+                                --sum(case when a.fecha = current_date + interval '2 day' then 1 else null end) as carga_2,
+                                --sum(case when a.fecha = current_date + interval '3 day' then 1 else null end) as carga_3,
                                 sum(case when t.rea_m = 1 then 1 else null end) as reas_m,
                                 sum(case when t.resumen_estado = 'no rea' then 1 else null end) as no_reas,
                                 sum(case when t.resumen_estado in ('incompleta', 'con problemas') then 1 else null end) as incompletas,
-                                sum(case when t.resumen_estado in ('vacia') then 1 else null end) as vacias,
-                                sum(case when a.fecha is not null and t.rea_m = 1 then 1 else null end)*1.0 / nullif(count(distinct a.fecha),0) as reas_dia
+                                sum(case when t.resumen_estado in ('vacia') then 1 else null end) as vacias
+                                --sum(case when a.fecha is not null and t.rea_m = 1 then 1 else null end)*1.0 / nullif(count(distinct a.fecha),0) as reas_dia
                             from tem t inner join areas a using (area)
                             where t.relevador = u.idper
                     ) t left join lateral (
