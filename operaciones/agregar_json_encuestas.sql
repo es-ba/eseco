3b7fa02a409715d06e6e9034798918ce04e57665
RaquelSalva <raquelzssalva@gmail.com>
2020-10-30 19:45:05 -0300

    bosquejo preseteo de variables para participacion2

--- operaciones/agregar_json_encuestas.sql
+++ operaciones/agregar_json_encuestas.sql
@@ -23,6 +23,16 @@ update tem
   )
   where json_encuesta is null and area>=900;
 
+/*
+update tem t
+  set json_encuesta=jsonb_set(t.json_encuesta,'{personas}',jsonb_build_array((a.json_encuesta->'personas'->((a.json_encuesta->>'p11')::integer - 1))))
+    ||jsonb_build_object('cp',1, 'p11',1, 'p12',a.json_encuesta->>'p12')
+  from tem_reas_eseco202_participacion2 a
+  where t.enc=a.enc and t.participacion=2 and t.clase='Rea202'; 
+-- quizas haya que presetear las preguntas ei y ci de contacto al final del cuestionario
+-- al probar, se despliegan valores preseteados de p11,p12 despues de poner si en p9
+-- cp? 1 o cp anterior? 
+*/
 
 insert into tareas_tem (operativo, enc, tarea, habilitada, operacion, fecha_asignacion,asignado)
   select operativo, enc, ta.tarea, true, operacion, fecha_asignacion,asignado
