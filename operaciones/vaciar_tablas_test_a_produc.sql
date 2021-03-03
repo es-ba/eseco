df703513864f78b685bdfacce020acf4cb05844b
manueldelapenna <manueldelapenna@gmail.com>
2021-03-03 12:25:46 -0300

    agregamos operaciones de limpieza de test a prod

---
+++ operaciones/vaciar_tablas_test_a_produc.sql
@@ -0,0 +1,10 @@
+set search_path= encu;
+set role to eseco211_produc_owner;
+
+delete from tareas_tem;
+delete from tareas_areas;
+delete from areas;
+delete from tem;
+
+delete from etiquetas;
+delete from planchas;
