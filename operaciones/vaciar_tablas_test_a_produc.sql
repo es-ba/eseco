cffe281cd1cb65df22b9cc0ab0f8a3ba17be9ca8
manueldelapenna <manueldelapenna@gmail.com>
2021-03-03 12:35:54 -0300

    agregamos transaccion

--- operaciones/vaciar_tablas_test_a_produc.sql
+++ operaciones/vaciar_tablas_test_a_produc.sql
@@ -1,6 +1,8 @@
 set search_path= encu;
 set role to eseco211_produc_owner;
 
+begin transaction;
+
 delete from tareas_tem;
 delete from tareas_areas;
 delete from tem;
@@ -8,3 +10,5 @@ delete from areas;
 
 delete from etiquetas;
 delete from planchas;
+
+commit;
