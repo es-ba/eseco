f4edb6117a40d9af17ae9dc3083c9f7ad062b464
RaquelSalva <raquelzssalva@gmail.com>
2021-07-08 17:56:57 -0300

    Cambios en script de nuevos casilleros

--- install/actualizar-2021-07-06-casilleros.sql
+++ install/actualizar-2021-07-06-casilleros.sql
@@ -1,6 +1,12 @@
 set search_path= encu;
 SET role eseco211_muleto_owner;
 
+delete from casilleros where id_casillero~'^P7/\d$' or id_casillero='P7';
+delete from casilleros c  
+  where id_casillero~'^V1_\d/\d$' ;
+delete from casilleros c  
+  where id_casillero~'^V1_' and tipoc in('P','FILTRO') ;
+
 --pregunta sobre dengue
 INSERT INTO casilleros (operativo, id_casillero, padre, tipoc, casillero, orden, nombre, tipovar, longitud, tipoe, aclaracion, salto, unidad_analisis, cantidad_resumen, irrepetible, despliegue, ver_id, optativo, formulario_principal, var_name, var_name_especial, expresion_habilitar, valor_ns_nc, valor_sin_dato, leer, calculada, libre, especial) VALUES
 ('ESECO','P7','LP','P','P7',170,'¿Alguna vez un médico o personal de salud le dijo que tenía dengue?','si_no',NULL,NULL,NULL,NULL,'personas',NULL,True,NULL,NULL,NULL,NULL,'p7',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
@@ -15,9 +21,8 @@ update casilleros set orden= orden + 20 where operativo='ESECO' AND id_casillero
 --nuevos casilleros sobre vacunacion
 INSERT INTO casilleros (operativo, id_casillero, padre, tipoc, casillero, orden, nombre, tipovar, longitud, tipoe, aclaracion, salto, unidad_analisis, cantidad_resumen, irrepetible, despliegue, ver_id, optativo, formulario_principal, var_name, var_name_especial, expresion_habilitar, valor_ns_nc, valor_sin_dato, leer, calculada, libre, especial) VALUES
   ('ESECO', 'V1_1', 'V', 'P', 'V1_1', 1408, '¿Cuántas dosis recibió?', 'opciones', NULL, NULL, NULL, NULL, NULL, NULL, true, 'si_leer', NULL, NULL, NULL, 'v1_1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-  ('ESECO', 'V1_2', 'V', 'P', 'V1_2', 1411, '¿En qué fecha recibió la primera dosis de la vacuna?', 'fecha', NULL, NULL, 'DD/MM/AAAA', NULL, NULL, NULL, true, NULL, NULL, NULL, NULL, 'v1_2', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-  ('ESECO', 'V_Filtro', 'V', 'FILTRO', 'V_Filtro', 1414, 'Aplicar si en la pregunta V1_1 contestó la opción 2. "Dos dosis". El resto pasa a pregunta V1_4', NULL, NULL, NULL, NULL, 'V1_4', NULL, NULL, true, NULL, NULL, NULL, NULL, NULL, NULL, 'v1_1=1', NULL, NULL, NULL, NULL, NULL, NULL),
-  ('ESECO', 'V1_3', 'V', 'P', 'V1_3', 1417, '¿En qué fecha recibió la segunda dosis de la vacuna?', 'fecha', NULL, NULL, 'DD/MM/AAAA', NULL, NULL, NULL, true, NULL, NULL, NULL, NULL, 'v1_3', NULL, 'v1_1=2', NULL, NULL, NULL, NULL, NULL, NULL),
+  ('ESECO', 'V1_2', 'V', 'P', 'V1_2', 1412, '¿En qué fecha recibió la primera dosis de la vacuna?', 'texto', NULL, NULL, 'DD/MM/AAAA', NULL, NULL, NULL, true, NULL, NULL, NULL, NULL, 'v1_2', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+  ('ESECO', 'V1_3', 'V', 'P', 'V1_3', 1417, '¿En qué fecha recibió la segunda dosis de la vacuna?', 'texto', NULL, NULL, 'DD/MM/AAAA', NULL, NULL, NULL, true, 'ocultar', NULL, NULL, NULL, 'v1_3', NULL, 'v1_1=2', NULL, NULL, NULL, NULL, NULL, NULL),
   ('ESECO', 'V1_4', 'V', 'P', 'V1_4', 1420, '¿Qué vacuna recibió?', 'opciones', NULL, NULL, NULL, NULL, NULL, NULL, true, 'si_leer', NULL, NULL, NULL, 'v1_4', NULL, 'v1=1', NULL, NULL, NULL, NULL, NULL, NULL),
   ('ESECO', 'V1_4_OTRA', 'V', 'P', 'V1_4_OTRA', 1423, '¿Cuál?', 'texto', NULL, NULL, NULL, NULL, NULL, NULL, true, 'no_leer ocultar', '-', NULL, NULL, 'v1_4_otra', NULL, 'v1_4=4', NULL, NULL, NULL, NULL, NULL, NULL),
   ('ESECO', 'V1_5', 'V', 'P', 'V1_5', 1425, '¿Dónde recibió la vacuna?', 'opciones', NULL, NULL, NULL, 'E1', NULL, NULL, true, 'si_leer', NULL, NULL, NULL, 'v1_5', NULL, 'v1=1', NULL, NULL, NULL, NULL, NULL, NULL),
