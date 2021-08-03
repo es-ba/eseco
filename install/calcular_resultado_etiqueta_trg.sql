69b4f897ec4fb73ca34f41b5255fe5e7ef6214cc
RaquelSalva <raquelzssalva@gmail.com>
2021-08-03 14:04:29 -0300

    Simplifico escritura del calculo de resultado, habiendo indagado todas las posibilidades hasta el momento.

--- install/calcular_resultado_etiqueta_trg.sql
+++ install/calcular_resultado_etiqueta_trg.sql
@@ -3,35 +3,28 @@ set role to eseco211_produc_owner;
 set search_path = encu;
 
 
-CREATE OR REPLACE FUNCTION calcular_resultado_etiqueta_trg() RETURNS trigger
+CREATE OR REPLACE FUNCTION calcular_resultado_etiqueta_trg() 
+  RETURNS trigger
   LANGUAGE plpgsql
 AS
 $BODY$
 begin
-  --si los dos son estrictamente null da null
   if new.resultado_n is null and new.resultado_s is null then 
-	new.resultado = null;
-	return new; 
+    --si los dos son estrictamente null da null
+    new.resultado = null;
+  elsif new.resultado_n = 'Positivo' or new.resultado_s = 'Positivo' then 
+    --si uno es positivo es positivo
+    new.resultado = 'Positivo'; 
+  elsif new.resultado_n = 'Negativo' and new.resultado_s = 'Negativo' then 
+    --solo es negativo si los dos son negativo  
+    new.resultado = 'Negativo' ;
+  else
+    --si los dos son escasa muestra o indeterminado queda indeterminado o solo uno de los dos es null
+    --if (new.resultado_n in ('Escasa muestra','Indeterminado', 'Negativo') or new.resultado_n is null) and 
+    --  (new.resultado_s in ('Escasa muestra','Indeterminado', 'Negativo') or new.resultado_s is null) then 
+    new.resultado = 'Indeterminado'; 
   end if;
-  
-  --si uno es positivo es positivo
-  if new.resultado_n = 'Positivo' or new.resultado_s = 'Positivo' then 
-	new.resultado = 'Positivo'; 
-	return new;
-  end if;
-  
-  --solo es negativo si los dos son negativo  
-  if new.resultado_n = 'Negativo' and new.resultado_s = 'Negativo' then 
-	new.resultado = 'Negativo' ;
-	return new;
-  end if;
-  
-  --si los dos son escasa muestra o indeterminado queda indeterminado o solo uno de los dos es null
-  if (new.resultado_n in ('Escasa muestra','Indeterminado', 'Negativo') or new.resultado_n is null) and 
-	(new.resultado_s in ('Escasa muestra','Indeterminado', 'Negativo') or new.resultado_s is null) then 
-	new.resultado = 'Indeterminado'; 
-	return new;
-  end if;
+  return new;
 end;
 $BODY$;
 
