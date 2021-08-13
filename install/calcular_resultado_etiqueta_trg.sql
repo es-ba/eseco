6b5fc561042ad74133fbfd9b1660bccaf4c1bfda
RaquelSalva <raquelzssalva@gmail.com>
2021-08-13 13:38:40 -0300

    Cambio en el calculo de resultado de etiqueta

--- install/calcular_resultado_etiqueta_trg.sql
+++ install/calcular_resultado_etiqueta_trg.sql
@@ -9,21 +9,7 @@ CREATE OR REPLACE FUNCTION calcular_resultado_etiqueta_trg()
 AS
 $BODY$
 begin
-  if new.resultado_n is null and new.resultado_s is null then 
-    --si los dos son estrictamente null da null
-    new.resultado = null;
-  elsif new.resultado_n = 'Positivo' or new.resultado_s = 'Positivo' then 
-    --si uno es positivo es positivo
-    new.resultado = 'Positivo'; 
-  elsif new.resultado_n = 'Negativo' and new.resultado_s = 'Negativo' then 
-    --solo es negativo si los dos son negativo  
-    new.resultado = 'Negativo' ;
-  else
-    --si los dos son escasa muestra o indeterminado queda indeterminado o solo uno de los dos es null
-    --if (new.resultado_n in ('Escasa muestra','Indeterminado', 'Negativo') or new.resultado_n is null) and 
-    --  (new.resultado_s in ('Escasa muestra','Indeterminado', 'Negativo') or new.resultado_s is null) then 
-    new.resultado = 'Indeterminado'; 
-  end if;
+  new.resultado= new.resultado_s; 
   return new;
 end;
 $BODY$;
@@ -31,10 +17,15 @@ $BODY$;
 -- /*
 drop trigger if exists calcular_resultado_etiqueta_trg on etiquetas;
 CREATE TRIGGER calcular_resultado_etiqueta_trg
-  BEFORE UPDATE OF resultado_s, resultado_n
+  BEFORE UPDATE OF resultado_s
   ON etiquetas
   FOR EACH ROW
   EXECUTE PROCEDURE calcular_resultado_etiqueta_trg();
 
 -- */
-
+/*
+update etiquetas
+  set resultado= resultado_s
+  --select count(*) from etiquetas
+  where resultado is distinct from resultado_s and fecha is not null and laboratorista is not null;
+*/  
