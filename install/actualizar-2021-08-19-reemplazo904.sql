c4930a4b0c842399a2dedd0064c8b7627a82db42
RaquelSalva <raquelzssalva@gmail.com>
2021-08-19 18:06:08 -0300

    Me faltó grabar el archivo! Va reemplazo del area 904

--- install/actualizar-2021-08-19-reemplazo904.sql
+++ install/actualizar-2021-08-19-reemplazo904.sql
@@ -1,21 +1,14 @@
 set search_path= encu;
 SET role eseco211_produc_owner;
 
-select * into norea_bkp from no_rea;
+select enc, nomcalle, nrocomuna, nrofraccion, nroradio, json_encuesta
+ from tem where area=904
+ order by 1;
 
 --select * from no_rea where grupo0~'ausentes y';
-update no_rea set grupo0='ausentes'  where  no_rea ~'^(7|75|12)$';
-update no_rea set grupo0='rechazos'  where  no_rea ~'^(8|81|82|83|85)$' and grupo0~'ausentes y';
-update no_rea set grupo0='otras causas' where no_rea='9';
+update tem 
+  set nomcalle='VILLA 20',
+      nrofraccion=7,
+      json_encuesta=json_encuesta||jsonb_build_object('ug2','VILLA 20','ug4',7)
+  where  area= 904;
 
-/*
-select count(*) from tem
-  where tipo_domicilio=1 and (json_encuesta->>'dv3' in ('7','12') or json_encuesta->>'sp6'='2');
-
-select count(*) from tem
-  where rea_m=1 and tipo_domicilio=1 and (json_encuesta->>'dv3' ='8' 
-  or json_encuesta->>'s1' ='2' or json_encuesta->>'s2' ='2' or json_encuesta->>'s3' ='2' or json_encuesta->>'sp6'='3');
-
-select count(*) from tem
-  where tipo_domicilio=1 and json_encuesta->>'dv3'='9';
-*/
